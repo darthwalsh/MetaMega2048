ca3115fa201e7363c92613218c8cd89f88c833bc
Carl Walsh <darthwalsh@gmail.com>
2015-09-05 21:21:29 -0700

    basic coloring

--- index.js
+++ index.js
@@ -3,6 +3,37 @@
 function $(id) { return document.getElementById(id); }
 function create(el) { return document.createElement(el); }
 
+// http://stackoverflow.com/a/8023734/771768
+// 0 <= h, s, v <= 1
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v; g = t; b = p; break;
+        case 1: r = q; g = v; b = p; break;
+        case 2: r = p; g = v; b = t; break;
+        case 3: r = p; g = q; b = v; break;
+        case 4: r = t; g = p; b = v; break;
+        case 5: r = v; g = p; b = q; break;
+    }
+    r = Math.round(r * 255);
+    g = Math.round(g * 255);
+    b = Math.round(b * 255);
+    return "#"+(r).toString(16)+(g).toString(16)+(b).toString(16);
+}
+
+function getColor(n) {
+  if (n == 0)
+    return "white";
+
+  var l = 15 - (Math.log2(n) % 10);
+  return HSVtoRGB(l/20, 0.5, 0.5);
+}
+  
 window.onload = function() {
   var table = $("grid");
 
@@ -25,8 +56,11 @@ window.onload = function() {
   
   var render = function() {
     for(var x = 0; x < 4; ++x)
-      for(var y = 0; y < 4; ++y)
-        grid[x][y].innerText = game.grid[x][y] || " ";
+      for(var y = 0; y < 4; ++y) {
+        var n = game.grid[x][y];
+        grid[x][y].innerText = n || " ";
+        grid[x][y].style.backgroundColor = getColor(n);
+      }
   };
 
   game.addElem();
